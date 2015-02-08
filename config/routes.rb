156aa33866e148185a8f7425f0c50684b6ce7e0c
Mixolidia Gautreaux <mixolidia@gmail.com>
2015-02-07 21:34:47 -0800

    Add code controller & views. Add links to home narrative.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   # navigation
     root  'home#home'
-    get   '/',                 to: 'home#home',     as: :home
+    get   '/',           to: 'home#home',     as: :home
+
+  # code
+    get   '/code',       to: 'code#code',     as: :code
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
