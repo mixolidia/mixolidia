aff2c9dd160fc86b31f1b444ad1428cf88c2b747
Mixolidia Gautreaux <mixolidia@gmail.com>
2015-02-08 01:27:09 -0800

    Add color shift on hover to home page narrative.

--- app/assets/javascripts/mxld.js
+++ app/assets/javascripts/mxld.js
@@ -1,7 +1,9 @@
 // social media & contact icons
-$(function(){
+$(function() {
      $("a img").hover(
           function(){this.src = this.src.replace(".png","n.png");},
           function(){this.src = this.src.replace("n.png",".png");
      });
 });
+
+//Mixolidia Gautreaux link
